27afc18b5465f7686e7e1a558bea4fdd4613bae8
Darsh99 <ajudiya.darshan007@gmail.com>
2020-01-08 13:01:31 +0530

    update check release

--- app/src/main/java/com/example/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sqliteexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +13,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     EditText name,roll,str;
-    Button add,show;
+    Button add,show,checkupdate;
     TextView dta;
     DatabaseHandler handler=new DatabaseHandler(this);
     @Override
@@ -27,6 +39,48 @@ public class MainActivity extends AppCompatActivity {
         add=(Button)findViewById(R.id.insert);
         show=(Button)findViewById(R.id.disp);
         dta=(TextView)findViewById(R.id.data);
+        checkupdate=findViewById(R.id.checkupdate);
+
+        checkupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    URL url=new URL("https://github.com/DarshanAjudiya/sqliteexample/raw/master/app/release/output.json");
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    InputStream stream=connection.getInputStream();
+                    BufferedReader reader=new BufferedReader(new InputStreamReader(stream));
+                    String line=reader.readLine();
+                    String data="";
+                    while(line!=null)
+                    {
+                        data+=line;
+                        line=reader.readLine();
+                    }
+                    System.out.println(data);
+                    JSONObject object=new JSONObject(data);
+                    int versioncode=object.getInt("verionCode");
+
+                    PackageInfo info=getApplicationContext().getPackageManager().getPackageInfo(getPackageName(),0);
+                    long appversioncode=-1;
+
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                        appversioncode = info.getLongVersionCode();
+                    }
+                    else {
+                        appversioncode = info.versionCode;
+                    }
+
+                    System.out.println("Current version:" +
+                            appversioncode+"\nserverappversion:"+versioncode);
+
+
+                } catch (IOException | JSONException | PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
